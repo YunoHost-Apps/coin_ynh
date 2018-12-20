f9bdeba4a54d7b694eae9db29a95501bc3a9647c
Josué Tille <josue@tille.ch>
2018-12-20 23:05:20 +0100

    Backup and restore database

--- scripts/common.sh
+++ scripts/common.sh
@@ -7,9 +7,7 @@ function install_dependencies()
 
 function init_db()
 {
-    sed -i '/local\s*all\s*all\s*peer/i \
-local all coin password' /etc/postgresql/9.*/main/pg_hba.conf
-    service postgresql reload
+    ynh_psql_test_if_first_run
     # Generate random password
     db_name=$app
     db_user=$app
