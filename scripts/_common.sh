596475fe4bb2a0ddd717abb91ff54896f11ed58a
ljf <ljf+github@grimaud.me>
2019-08-24 18:21:09 +0200

    [fix] Missing dependencies

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="gunicorn python-dev python-pip libldap2-dev libpq-dev libsasl2-dev libjpeg-dev libxml2-dev libxslt1-dev libffi-dev python-cairo libpango1.0-0 postgresql postgresql-contrib"
+pkg_dependencies="python-virtualenv gunicorn python-dev python-pip libldap2-dev libpq-dev libsasl2-dev libjpeg-dev libxml2-dev libxslt1-dev libffi-dev python-cairo libpango1.0-0 postgresql postgresql-contrib"
 
 #=================================================
 # PERSONAL HELPERS
