c2bd064442c0052782dd68fc7583fd844083ed6e
Alexandre Aubin <alex.aubin@mailoo.org>
2020-06-30 20:47:24 +0200

    Fixes for buster

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="python-virtualenv gunicorn python-dev python-pip libldap2-dev libpq-dev libsasl2-dev libjpeg-dev libxml2-dev libxslt1-dev libffi-dev python-cairo libpango1.0-0 postgresql postgresql-contrib"
+pkg_dependencies="python-virtualenv gunicorn python-dev python-pip libldap2-dev libpq-dev libsasl2-dev libjpeg-dev libxml2-dev libxslt1-dev libffi-dev python-cairo libpango1.0-0 postgresql postgresql-contrib postgresql-server-dev-11"
 
 #=================================================
 # PERSONAL HELPERS
