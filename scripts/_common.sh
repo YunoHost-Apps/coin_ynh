48a4aa8b8d837a5a8c97ed7eeb3aac408f484bee
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-11-16 17:15:00 +0100

    v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,9 +6,6 @@
 
 python_version=2.7.18
 
-# dependencies used by the app
-#REMOVEME? pkg_dependencies="gunicorn libldap2-dev libpq-dev libsasl2-dev libjpeg-dev libxml2-dev libxslt1-dev libffi-dev libpango1.0-0 postgresql postgresql-contrib postgresql-server-dev-13"
-
 #=================================================
 # PERSONAL HELPERS
 #=================================================
