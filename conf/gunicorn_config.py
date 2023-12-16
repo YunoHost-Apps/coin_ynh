affabb064a1eb92f122433f9cef89feaabeb0d4c
Alexandre Aubin <alex.aubin@mailoo.org>
2023-12-16 17:15:48 +0100

    Zelrmvlf

--- conf/gunicorn_config.py
+++ conf/gunicorn_config.py
@@ -1,8 +1,8 @@
-command = '__YNH_PYTHON_PATH__/gunicorn'
-pythonpath = '/opt/__APP__'
+command = '__INSTALL_DIR/venv/bin/gunicorn'
+pythonpath = '__INSTALL_DIR__'
 workers = 4
 user = '__APP__'
-bind = 'unix:/opt/__APP__/sock'
+bind = 'unix:__INSTALL_DIR__/sock'
 pid = '/run/gunicorn/__APP__-pid'
 errorlog = '/var/log/__APP__/error.log'
 accesslog = '/var/log/__APP__/access.log'
