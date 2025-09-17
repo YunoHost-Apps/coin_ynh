e08eca508b1114077ece688f59fa012136c1cdca
Josué Tille <josue@tille.ch>
2025-09-17 07:28:42 +0200

    Upstream improve ldap group sync

--- sources/django_ldap_extension.py
+++ sources/django_ldap_extension.py
@@ -1,10 +1,23 @@
-from django_auth_ldap.config import LDAPGroupType
+from django_auth_ldap.config import LDAPGroupType, MemberDNGroupType
+
+
+class MemberDNGroupTypeWithoutPrimaryGroup(MemberDNGroupType):
+    def __init__(self, member_attr, name_attr="cn"):
+        super().__init__(member_attr, name_attr)
+
+    def user_groups(self, ldap_user, group_search):
+        result = super().user_groups(ldap_user, group_search)
+        # We exclude the primary group of the user
+        result = {k: v
+                  for k, v in result
+                  if k != f'cn={ldap_user._username},ou=groups,dc=yunohost,dc=org'}.items()
+        return result
+
 
 class MemberDNGroupTypeUnion(LDAPGroupType):
-
     def __init__(self, *types, name_attr='cn'):
         self.types = types
-        super(MemberDNGroupTypeUnion, self).__init__(name_attr)
+        super().__init__(name_attr)
 
     def user_groups(self, ldap_user, group_search):
         res = dict()
